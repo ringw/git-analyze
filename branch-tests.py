556f9a32f7cb1131f4be1e66262088e81788f974
Dan Ringwalt <dringw@gmail.com>
2015-08-14 19:09:20 -0400

    Branch test runner

--- branch-tests.py
+++ branch-tests.py
@@ -7,10 +7,10 @@ import sys
 import time
 
 REPO = os.getcwd()
-GITROOT = os.path.join(REPO, 'git')
+GITROOT = os.path.join(REPO, '.git')
 if not os.path.exists(GITROOT):
     GITROOT = REPO
-PIDFILE = os.path.join(GITROOT, 'info', 'git-branch-tests.pid')
+PIDFILE = os.path.join(GITROOT, 'info', 'git-stats.pid')
 PID = os.getpid()
 while True: # Spinlock while another branch-tests process is running
     if not os.path.exists(PIDFILE):
@@ -30,8 +30,32 @@ try:
             continue
         BRANCH = os.path.basename(name)
         CLONE = tempfile.mkdtemp(prefix='branchtest-%s-' % BRANCH)
-        subprocess.check_output(['git', 'clone', '--branch', BRANCH,
-                                 REPO, CLONE])
+        subprocess.check_call(['git', 'clone', '--branch', BRANCH,
+                               REPO, CLONE])
+
+        cmd_output = subprocess.check_output(['git', 'reflog'], cwd=CLONE)
+        COMMIT = cmd_output.split(' ', 1)[0]
+
+        subprocess.check_call(['git', 'checkout', 'master',
+                               '--', 'tests/git-stats'], cwd=CLONE)
+        TESTS_PATH = os.path.join(CLONE, 'tests', 'git-analyze')
+
+        output_dir = os.path.join(GITROOT, 'info', 'git-analyze')
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+        for test_file in os.path.listdir(TESTS_PATH):
+            full_path = os.path.join(TESTS_PATH, test_file)
+            try:
+                output = subprocess.check_output([full_path], cwd=CLONE)
+                commit_dir = os.path.join(output_dir, COMMIT)
+                if not os.path.exists(commit_dir):
+                    os.mkdir(commit_dir)
+                test_name = os.path.splitext(test_file)[0]
+                output_path = os.path.join(commit_dir, test_name)
+                with output_fh as open(output_path, 'wb'):
+                    output_fh.write(output)
+
         shutil.rmtree(CLONE)
         CLONE = None
 finally:
